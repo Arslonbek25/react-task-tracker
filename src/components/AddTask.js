b02ff2c72c7bd434d51f7791b85970fc54edf9ae
Arslonbek25 <amadyarov25@gmail.com>
2023-04-23 15:16:21 +0500

    Update UI

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,20 +1,40 @@
+import { useState } from "react";
+
 const AddTask = () => {
+	const [text, setText] = useState("");
+	const [day, setDay] = useState("");
+	const [reminder, setReminder] = useState(false);
+
 	return (
 		<form className="add-form">
 			<div className="form-control">
 				<label>Task</label>
-				<input type="text" placeholder="Add Task" />
+				<input
+					value={text}
+					onChange={e => setText(e.target.value)}
+					type="text"
+					placeholder="Add Task"
+				/>
 			</div>
 			<div className="form-control">
 				<label>Day & Time</label>
-				<input type="text" placeholder="Add Day & Time" />
+				<input
+					value={day}
+					onChange={e => setDay(e.target.value)}
+					type="text"
+					placeholder="Add Day & Time"
+				/>
 			</div>
 			<div className="form-control form-control-check">
 				<label>Set Reminder</label>
-				<input type="checkbox" />
+				<input
+					value={reminder}
+					onChange={e => setReminder(e.target.checked)}
+					type="checkbox"
+				/>
 			</div>
 
-            <input type="submit" value="Save Task" className="btn btn-block" />
+			<input type="submit" value="Save Task" className="btn btn-block" />
 		</form>
 	);
 };
